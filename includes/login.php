fe0410b1c3431f9f6a432766b1bc8d5a83682940
Mohammed Shurrab <mohmmedshurrab07@gmail.com>
2022-12-15 23:58:16 +0200

    Created a login system & personalized admin page

---
+++ includes/login.php
@@ -0,0 +1,46 @@
+<?php include("db.php"); ?>
+<?php session_start(); ?>
+
+<?php 
+
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $query = "select * from users where username  = '$username' and user_password = '$password' ";
+    $select_user_query = mysqli_query($connection , $query);
+    if(!$select_user_query){
+
+        die("query failed" . mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_assoc($select_user_query)){
+
+         $user_id = $row['user_id'];
+         $user_username = $row['username'];
+         $user_password = $row['user_password'];
+         $user_firstname = $row['user_firstname'];
+         $user_lastname = $row['user_lastname'];
+         $user_role = $row['user_role'];
+        
+    }
+
+    if ($username !== $user_username && $password !== $user_password) {
+       header("Location: ../index.php");
+    } else if ($username == $user_username && $password == $user_password ){
+        $_SESSION['username'] = $user_username;
+        $_SESSION['firstname'] = $user_firstname;
+        $_SESSION['lastname'] = $user_lastname;
+        $_SESSION['role'] = $user_role;
+        header("Location: ../admin");
+        
+    }else{
+        header("Location: ../index.php");
+    }
+}
+
+?>
